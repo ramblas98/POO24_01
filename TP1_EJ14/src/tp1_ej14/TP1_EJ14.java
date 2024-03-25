ab2d8ebbc38932d2a7772ba9f213f7516ee5f639
usuario <usuario@DESKTOP-OES5E7K>
2024-03-25 17:37:58 -0300

    Primera version

--- TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
+++ TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
@@ -8,13 +8,34 @@ package tp1_ej14;
  *
  * @author profeblas
  */
+
+import java.util.*;
+
 public class TP1_EJ14 {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner teclado = new Scanner(System.in);
+        int N, QT, S ,may,i;
+        int V[];
+        int Dif[];
+        
+        N= teclado.nextInt();
+        V = new int[N];
+        Dif = new int[N];
+        
+        for(i=1;i<=N;i++){
+            QT= teclado.nextInt();
+            S = teclado.nextInt();
+            
+            for(j=1;j<=)
+            
+        }
     }
     
 }
