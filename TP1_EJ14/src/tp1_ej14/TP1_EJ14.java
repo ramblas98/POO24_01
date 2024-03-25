084db0c63e0b08a9ac56144391110ca75f94db22
Gusano <Gusano@GUS>
2024-03-25 17:15:07 -0300

    Revert "Ejercicio 14"
    This reverts commit 1d6c3f0a24db3610c860faeca7df7b78a068ab77.

--- TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
+++ TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
@@ -1,23 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package tp1_ej14;
-import java.util.*;
 
+/**
+ *
+ * @author profeblas
+ */
 public class TP1_EJ14 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        int n, m, i, j, v[], c, g;
-        n = teclado.nextInt();
-        for (i=0; i<n; i++) {
-            g = 0;
-            m = teclado.nextInt();
-            c = teclado.nextInt();
-            v = new int[m];
-            for (j=0; j<m; j++) v[j] = teclado.nextInt();
-            for (j=0; j<m; j++) {
-                if (v[j]==c) {g = j; break;}
-                else if (Math.abs(c-v[j]) < Math.abs(c-v[g])) g = j;
-            }
-            System.out.println(g+1);
-        }
-        System.out.println("Adios mundo");        
-    }   
+        // TODO code application logic here
+    }
+    
 }
