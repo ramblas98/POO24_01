b6b34b8ca33de14fc42c0c18374420ac54674f53
magal <magal@AT560>
2024-03-25 17:14:44 -0300

    cambio de autor

--- TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
+++ TP1_EJ14/src/tp1_ej14/TP1_EJ14.java
@@ -4,9 +4,10 @@
  */
 package tp1_ej14;
 
+import java.util.Scanner;
 /**
  *
- * @author profeblas
+ * @author magali.17.reina@gmail.com
  */
 public class TP1_EJ14 {
 
@@ -15,6 +16,41 @@ public class TP1_EJ14 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int N, numeros[], S, QT, i, k, p, menor, pos, diferencias[];
+        
+        N = teclado.nextInt();
+        numeros = new int[10];
+        
+        
+        for(i = 0; i < N; i++){
+            QT = (int)(Math.random()*(10-4+1)+4); //Cantidad de integrantes
+            S = (int)(Math.random()*(100-1+1)+1); //El número a adivinar
+            for(k = 0; k < QT; k++){
+                numeros[k] = (int)(Math.random()*(100-1+1)+1);//Número de c/integrante
+            }
+         
+            System.out.println("Cantidad de integrantes: " + QT);
+            System.out.println("Numero a adivinar: " + S);
+            System.out.println("Vector generado:");
+            
+            for(k = 0; k < QT; k++){
+                System.out.print(numeros[k] + " ");
+            }
+            System.out.println();
+            diferencias = new int[QT];
+            for(p = 0; p < QT; p++){
+                diferencias[p] = Math.abs(S - numeros[p]);
+            }
+            menor = diferencias[0];
+            pos = 0;
+            for(k = 1; k < QT; k++){
+                if(diferencias[k] < menor){
+                    menor = diferencias[k];
+                    pos = k;
+                }
+            }
+            System.out.println(pos+1);
+        }
     }
-    
 }
