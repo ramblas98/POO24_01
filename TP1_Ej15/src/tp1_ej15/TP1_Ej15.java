5223221f195283b8a2dcf4657e75405ff6c2d7bc
elcho <elcho@AaronZerda>
2024-03-27 16:14:22 -0300

    nuevo

---
+++ TP1_Ej15/src/tp1_ej15/TP1_Ej15.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_ej15;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author elcho
+ */
+public class TP1_Ej15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
